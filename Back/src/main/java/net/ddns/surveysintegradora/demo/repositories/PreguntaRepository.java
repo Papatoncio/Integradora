35b83cf36bb87e3242fddf01f6f6ecf1679062b6
Papatoncio <juanpij23@hotmail.com>
2022-08-09 18:19:16 -0500

    yes

--- Back/src/main/java/net/ddns/surveysintegradora/demo/repositories/PreguntaRepository.java
+++ Back/src/main/java/net/ddns/surveysintegradora/demo/repositories/PreguntaRepository.java
@@ -4,8 +4,9 @@ import net.ddns.surveysintegradora.demo.models.PreguntaModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins="http://localhost:4200/")
+@CrossOrigin (origins="http://localhost:4200/")
 @Repository
 @RepositoryRestResource(path = "preguntas", collectionResourceRel = "preguntas")
 public interface PreguntaRepository extends CrudRepository<PreguntaModel, Integer> {
